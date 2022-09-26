fbb563e5da5083c79c64f929ac7885c1c7e825a6
Astrid Hecht <silentaviary@gmail.com>
2022-09-26 12:00:23 -0600

    halfway thru extentsion

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,12 @@ class Doctor < ApplicationRecord
   belongs_to :hospital
   has_many :doctor_patients
   has_many :patients, through: :doctor_patients
+
+  def self.sort_by_patients
+    Doctor.group(:id).order('patients.count DESC')
+  end
+  
+  def patient_count
+    patients.count
+  end
 end
