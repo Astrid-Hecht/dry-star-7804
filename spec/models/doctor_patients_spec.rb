12a1ea88116a4007de6de3a6a2a10cb112d12140
Astrid Hecht <silentaviary@gmail.com>
2022-09-26 11:32:54 -0600

    test: add model tests, doctor_patients was missing one

--- spec/models/doctor_patients_spec.rb
+++ spec/models/doctor_patients_spec.rb
@@ -3,4 +3,35 @@ require 'rails_helper'
 RSpec.describe DoctorPatient do
   it {should belong_to :doctor}
   it {should belong_to :patient}
+
+  let!(:last_light) {Hospital.create!(name:'Last Light of Baleros')}
+  let!(:shh) {Hospital.create!(name:'Sacred Heart Hospital')}
+
+  let!(:geneva) {last_light.doctors.create!(name: 'Dr. Geneva', specialty: 'Trauma', university: 'Some School State')}
+  let!(:house) {last_light.doctors.create!(name: 'House MD', specialty: 'Who knows', university: 'Some Other State')}
+  let!(:braff) {shh.doctors.create!(name: 'Snack Braff', specialty: 'Wrestling', university: 'WV School of Medical Dance')}
+
+  let!(:ken) {geneva.patients.create!(name: 'Ken', age: 22)}
+  let!(:jordie) {geneva.patients.create!(name: 'Jordie', age: 22)}
+  let!(:neil) {house.patients.create!(name: 'Neil', age: 75)}
+  let!(:mary) {braff.patients.create!(name: 'Mary', age: 17)}
+
+  describe 'class methods' do 
+    describe '#find_match' do
+      it 'finds one doctorpatient relation obj' do
+        expect(DoctorPatient.find_match(geneva.id, ken.id).count).to eq(1)
+      end
+
+      it 'finds the correct relation obj' do
+        expect(DoctorPatient.find_match(geneva.id, ken.id)).to eq(DoctorPatient.where(doctor_id: geneva.id, patient_id: ken.id).first)
+        expect(DoctorPatient.find_match(geneva.id, jordie.id)).to eq(DoctorPatient.where(doctor_id: geneva.id, patient_id: jordie.id ).first)
+        expect(DoctorPatient.find_match(house.id, neil.id)).to eq(DoctorPatient.where(doctor_id: house.id, patient_id: neil.id).first)
+        expect(DoctorPatient.find_match(braff.id, mary.id)).to eq(DoctorPatient.where(doctor_id: braff.id, patient_id: mary.id).first)
+      end
+
+      it 'returns nil if no relationship exists' do
+        expect(DoctorPatient.find_match(geneva.id, mary.id)).to eq(nil)
+      end
+    end
+  end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -3,4 +3,31 @@ require 'rails_helper'
 RSpec.describe Patient do
   it {should have_many :doctor_patients}
   it {should have_many(:doctors).through(:doctor_patients) }
+
+  let!(:last_light) {Hospital.create!(name:'Last Light of Baleros')}
+  let!(:shh) {Hospital.create!(name:'Sacred Heart Hospital')}
+
+  let!(:geneva) {last_light.doctors.create!(name: 'Dr. Geneva', specialty: 'Trauma', university: 'Some School State')}
+  let!(:house) {last_light.doctors.create!(name: 'House MD', specialty: 'Who knows', university: 'Some Other State')}
+  let!(:braff) {shh.doctors.create!(name: 'Snack Braff', specialty: 'Wrestling', university: 'WV School of Medical Dance')}
+
+  let!(:ken) {geneva.patients.create!(name: 'Ken', age: 22)}
+  let!(:jordie) {geneva.patients.create!(name: 'Jordie', age: 22)}
+  let!(:neil) {house.patients.create!(name: 'Neil', age: 75)}
+  let!(:mary) {braff.patients.create!(name: 'Mary', age: 17)}
+
+  describe 'class methods' do
+    describe '#list_adults_alpha' do
+      it 'should only return patients above 18' do
+        expect(Patient.list_adults_alpha.include?(ken)).to be(true)
+        expect(Patient.list_adults_alpha.include?(jordie)).to be(true)
+        expect(Patient.list_adults_alpha.include?(neil)).to be(true)
+        expect(Patient.list_adults_alpha.include?(mary)).to be(false)
+      end
+
+      it 'should return them in alphabetical order' do
+        expect(Patient.list_adults_alpha).to eq([jordie,ken,neil])
+      end
+    end
+  end
 end
